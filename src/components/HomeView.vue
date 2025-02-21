8fb61e726b2b41c4dc4f4baa1cf10dfa3101dbf2
valitovgaziz <valitovgaziz@gmail.com>
2025-02-21 08:58:44 +0500

    Init commit. Перезапуск проекта со сборщиком проекта babel

---
+++ src/components/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main">
+        <p>this is home view</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeView',
+    setup() {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
