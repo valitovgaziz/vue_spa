24a30a95307770867295e70985a67d8dfc51a646
valitovgaziz <valitovgaziz@gmail.com>
2025-02-22 01:25:55 +0500

    Установил vue-router, vuex

---
+++ src/components/mainButtonComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button>
+
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
